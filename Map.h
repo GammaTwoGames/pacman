959c586560a48134308eb51199bf976bd173ebbe
GammaTwoGames <44450679+GammaTwoGames@users.noreply.github.com>
2019-04-06 14:15:50 +0300

    Улучшил визуализацию стен
    Улучшил визуализацию стен

--- Map.h
+++ Map.h
@@ -4,7 +4,7 @@
 int MAP[N][N] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1},
+    {1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1},
     {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
     {1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1},
     {1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1},
@@ -47,7 +47,9 @@ int MAP[N][N] = {
 
 class Map
 {
+private:
     int map[N][N];
+    void draw_rect(sf::RenderWindow* window,int i, int j, int k);
 public:
     Map();
     void draw(sf::RenderWindow* window);
@@ -64,6 +66,35 @@ Map::Map()
     }
 }
 
+void Map::draw_rect(sf::RenderWindow* window,int i, int j, int k)
+{
+    float thick = 0.2;
+    sf::RectangleShape shape;
+    shape.setFillColor(sf::Color::Blue);
+    if (k == 0)
+    {
+        shape.setSize(sf::Vector2f(X, X*thick));
+        shape.setPosition(X*i, X*j);
+    }
+    if (k == 1)
+    {
+        shape.setSize(sf::Vector2f(X*thick, X));
+        shape.setPosition(X*i + X*(1 - thick), X*j);
+    }
+    if (k == 2)
+    {
+        shape.setSize(sf::Vector2f(X, X*thick));
+        shape.setPosition(X*i, X*j + X*(1 - thick));
+    }
+    if (k == 3)
+    {
+        shape.setSize(sf::Vector2f(X*thick, X));
+        shape.setPosition(X*i, X*j);
+    }
+    shape.setOutlineThickness(0);
+    window -> draw(shape);
+}
+
 void Map::draw(sf::RenderWindow* window)
 {
     for(int i = 0; i < N; i++)
@@ -72,14 +103,22 @@ void Map::draw(sf::RenderWindow* window)
         {
             if(map[i][j] == 1)
             {
-                sf::RectangleShape shape;
-                shape.setSize(sf::Vector2f(X - 1, X - 1));
-                shape.setFillColor(sf::Color::Blue);
-                shape.setPosition(X*i, X*j);
-                window -> draw(shape);
+                if (j - 1 >= 0)
+                    if (map[i][j - 1] != 1)
+                        draw_rect(window, i, j, 0);
+                if (i + 1 < N)
+                    if (map[i + 1][j] != 1)
+                        draw_rect(window, i, j, 1);
+                if (j + 1 < N)
+                    if (map[i][j + 1] != 1)
+                        draw_rect(window, i, j, 2);
+                if (i - 1 >= 0)
+                    if (map[i - 1][j] != 1)
+                        draw_rect(window, i, j, 3);
             }
         }
     }
 }
 
 #endif /* Map_h */
+
