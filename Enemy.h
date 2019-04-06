fc35ac5b349bb2c25d6555df3097c6755f2a6a5c
GammaTwoGames <44450679+GammaTwoGames@users.noreply.github.com>
2019-04-06 16:04:41 +0300

    Улучшил мозги - нон стоп погоня
    Улучшил мозги - нон стоп погоня

--- Enemy.h
+++ Enemy.h
@@ -1,26 +1,36 @@
 #ifndef Enemy_h
 #define Enemy_h
 
+using namespace std;
+
 class Enemy : public Character
 {
-    sf::Texture texture;
-public:
-    Enemy();
-    void init(int _xi, int _yi);
-    void draw(sf::RenderWindow* window);
+private:
+    bool looking();
+    int pifagor(int p_xi, int p_yi, int dx, int dy);
     void move_left();
     void move_down();
     void move_right();
     void move_up();
+    sf::Texture texture;
+public:
+    Enemy();
+    int good_way(int p_xi, int p_yi);
+    void init(int _xi, int _yi);
+    void draw(sf::RenderWindow* window);
     void move(int p_xi, int p_yi);
     void check_fail(double p_x, double p_y);
+    int get_xi()
+    {
+        return looking();
+    }
 };
 
 Enemy::Enemy()
 {
     r = X/2;
     count = 100;
-    mov = 0;
+    mov = 20;
     texture.loadFromFile("res/ghost.jpeg", sf::IntRect(0, 0, 500, 500));
 }
 void Enemy::init(int _xi, int _yi)
@@ -30,6 +40,8 @@ void Enemy::init(int _xi, int _yi)
     y = _yi*X + X/2;
     yi = _yi;
 }
+
+
 void Enemy::draw(sf::RenderWindow* window)
 {
     sf::RectangleShape shape;
@@ -51,8 +63,9 @@ void Enemy::move_left()
     {
         --xi;
         count = 100;
-        mov = 0;
+        mov += 20;
     }
+    //cout<<"done";
 
 }
 void Enemy::move_down()
@@ -66,7 +79,7 @@ void Enemy::move_down()
     {
         ++yi;
         count = 100;
-        mov = 0;
+        mov += 20;
     }
 
 }
@@ -81,7 +94,7 @@ void Enemy::move_right()
     {
         ++xi;
         count = 100;
-        mov = 0;
+        mov += 20;
     }
 
 }
@@ -96,33 +109,96 @@ void Enemy::move_up()
     {
         --yi;
         count = 100;
-        mov = 0;
+        mov += 20;
     }
 
 }
+
+bool Enemy::looking()
+{
+    bool rb = 1;
+    if (((MAP[xi - 1][yi] == 1) && (MAP[xi + 1][yi] == 1)) && ((MAP[xi][yi - 1] == 0) && (MAP[xi][yi + 1] == 0))) rb = 0;
+    if (((MAP[xi - 1][yi] == 0) && (MAP[xi + 1][yi] == 0)) && ((MAP[xi][yi - 1] == 1) && (MAP[xi][yi + 1] == 1))) rb = 0;
+    return rb;
+}
+
+int Enemy::pifagor(int p_xi, int p_yi, int dx, int dy)
+{
+    int real_length;
+    real_length = (p_xi - xi)*(p_xi - xi) + (p_yi - yi)*(p_yi - yi);
+    int future_length;
+    future_length = (p_xi - xi - dx)*(p_xi - xi - dx) + (p_yi - yi - dy)*(p_yi - yi - dy);
+    return real_length - future_length;
+}
+
+int Enemy::good_way(int p_xi, int p_yi)
+{
+    int ans = 0;
+    int dl = - 1e6;
+    if (pifagor(p_xi, p_yi, 0, - 1) > dl) {ans = 1; dl = pifagor(p_xi, p_yi, 0, - 1);}
+    if (pifagor(p_xi, p_yi, 0,   1) > dl) {ans = 2; dl = pifagor(p_xi, p_yi, 0,   1);}
+    if (pifagor(p_xi, p_yi, 1,   0) > dl) {ans = 3; dl = pifagor(p_xi, p_yi, 1,   0);}
+    if (pifagor(p_xi, p_yi,-1,   0) > dl) {ans = 4; dl = pifagor(p_xi, p_yi,-1,   0);}
+    return ans;
+}
+
 void Enemy::move(int p_xi, int p_yi)
 {
-    if(mov == 0)
+    if (mov > 10)
     {
-        if(p_yi - yi < 0 && MAP[xi][yi - 1] != 1)
+        if (looking() == 1)
         {
-            mov = 1;
-            move_up();
+            int gw = good_way(p_xi, p_yi);
+            cout<<"tuta"<<gw<<" "<<MAP[xi - 1][yi]<<endl;
+            if((gw == 1) && (MAP[xi][yi - 1] != 1))
+            {
+                mov = 1;
+                move_up();
+            }
+            if((gw == 2) && MAP[xi][yi + 1] != 1)
+            {
+                mov = 2;
+                move_down();
+            }
+            if((gw == 3) && MAP[xi + 1][yi] != 1)
+            {
+                mov = 3;
+                move_right();
+            }
+            if((gw == 4)  && MAP[xi - 1][yi] != 1)
+            {
+                mov = 4;
+                move_left();
+            }
+
+            if (mov > 10)
+            {
+                if(p_yi - yi < 0 && MAP[xi][yi - 1] != 1)
+                {
+                    mov = 1;
+                    move_up();
+                }
+                else if(p_yi - yi > 0 && MAP[xi][yi + 1] != 1)
+                {
+                    mov = 2;
+                    move_down();
+                }
+                else if(p_xi - xi > 0 && MAP[xi + 1][yi] != 1)
+                {
+                    mov = 3;
+                    move_right();
+                }
+                else if(p_xi - xi < 0 && MAP[xi - 1][yi] != 1)
+                {
+                    mov = 4;
+                    move_left();
+
+                }
+            }
         }
-        else if(p_yi - yi > 0 && MAP[xi][yi + 1] != 1)
+        else
         {
-            mov = 2;
-            move_down();
-        }
-        else if(p_xi - xi > 0 && MAP[xi + 1][yi] != 1)
-        {
-            mov = 3;
-            move_right();
-        }
-        else if(p_xi - xi < 0 && MAP[xi - 1][yi] != 1)
-        {
-            mov = 4;
-            move_left();
+            mov -= 10;
         }
     }
     else
