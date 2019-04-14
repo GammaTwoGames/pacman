f75c21b61d6d0bd03317adaf822e9bc033b4f8ca
GammaTwoGames <44450679+GammaTwoGames@users.noreply.github.com>
2019-04-14 22:20:34 +0300

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -1,30 +1,36 @@
 #ifndef Enemy_h
 #define Enemy_h
 
+
+#include <SFML/Graphics.hpp>
+
 using namespace std;
 
 class Enemy : public Character
 {
 private:
     //int ans[];
+    int disgrace;
     bool looking();
     int pifagor(int p_xi, int p_yi, int dx, int dy);
-    void move_left();
-    void move_down();
-    void move_right();
-    void move_up();
-    bool move_simple(int dr);
-    sf::Texture texture;
+    void move_left(float time);
+    void move_down(float time);
+    void move_right(float time);
+    void move_up(float time);
+    bool move_simple(int dr, float time);
+    sf::Texture texture1;
+    sf::Texture texture2;
 public:
     Enemy();
     int good_way(int p_xi, int p_yi, int n);
     void init(int _xi, int _yi);
     void draw(sf::RenderWindow* window);
-    void move(int p_xi, int p_yi);
+    void move(int p_xi, int p_yi, float time);
     void check_fail(double p_x, double p_y);
-    int get_xi()
+
+    void make_disgrace()
     {
-        return looking();
+        disgrace = 1000;
     }
 };
 
@@ -33,7 +39,9 @@ Enemy::Enemy()
     r = X/2;
     count = 100;
     mov = 20;
-    texture.loadFromFile("res/ghost.jpeg", sf::IntRect(0, 0, 500, 500));
+    texture1.loadFromFile("res/ghost.jpeg", sf::IntRect(0, 0, 500, 500));
+    texture2.loadFromFile("res/ghost2.jpeg", sf::IntRect(0, 0, 500, 500));
+    disgrace = 0;
 }
 void Enemy::init(int _xi, int _yi)
 {
@@ -41,6 +49,10 @@ void Enemy::init(int _xi, int _yi)
     xi = _xi;
     y = _yi*X + X/2;
     yi = _yi;
+
+    count = 100;
+    mov = 21;
+
 }
 
 
@@ -51,67 +63,76 @@ void Enemy::draw(sf::RenderWindow* window)
     //shape.setFillColor(sf::Color::Red);
     shape.setPosition(x, y);
     shape.setOrigin(r, r);
-    shape.setTexture(&texture);
+    if (disgrace == 0)
+        shape.setTexture(&texture1);
+    else
+        shape.setTexture(&texture2);
     window -> draw(shape);
 }
-void Enemy::move_left()
+
+void Enemy::move_left(float time)
 {
     if(count > 0)
     {
-        x -= 0.4;
-        --count;
+        x -= 0.4 * time;
+        count -= time;
     }
     else
     {
         --xi;
         count = 100;
         mov += 20;
+
+        x = (xi)*X + 20;
     }
     //cout<<"done";
 
 }
-void Enemy::move_down()
+void Enemy::move_down(float time)
 {
     if(count > 0)
     {
-        y += 0.4;
-        --count;
+        y += 0.4*time;
+        count -= time;
     }
     else
     {
         ++yi;
         count = 100;
         mov += 20;
+        y = yi*X + 20;
     }
 
 }
-void Enemy::move_right()
+void Enemy::move_right(float time)
 {
     if(count > 0)
     {
-        x += 0.4;
-        --count;
+        x += 0.4*time;
+        count -= time;
     }
     else
     {
         ++xi;
         count = 100;
         mov += 20;
+        x = xi*X + 20;
     }
 
 }
-void Enemy::move_up()
+void Enemy::move_up(float time)
 {
     if(count > 0)
     {
-        y -= 0.4;
-        --count;
+        y -= 0.4*time;
+        count -= time;
     }
     else
     {
         --yi;
         count = 100;
         mov += 20;
+        y = yi*X + 20;
     }
 
 }
@@ -162,81 +183,133 @@ int Enemy::good_way(int p_xi, int p_yi, int n)
     return ans[n];
 }
 
-bool Enemy::move_simple(int dr)
+bool Enemy::move_simple(int dr, float time)
 {
         bool ret = 0;
-        if((dr == 1) && (MAP[xi][yi - 1] != 1))
+        if((dr == 1) && (MAP[xi][yi - 1] != 1) && (MAP[xi][yi - 1] != 9))
         {
             mov = 1;
-            move_up();
+            move_up(time);
             ret = 1;
         }
-        if((dr == 2) && MAP[xi][yi + 1] != 1)
+        if((dr == 2) && MAP[xi][yi + 1] != 1 && MAP[xi][yi + 1] != 9)
         {
             mov = 2;
-            move_down();
+            move_down(time);
             ret = 1;
         }
-        if((dr == 3) && MAP[xi + 1][yi] != 1)
+        if((dr == 3) && MAP[xi + 1][yi] != 1 && MAP[xi + 1][yi] != 9)
         {
             mov = 3;
-            move_right();
+            move_right(time);
             ret = 1;
         }
-        if((dr == 4)  && MAP[xi - 1][yi] != 1)
+        if((dr == 4)  && MAP[xi - 1][yi] != 1 && MAP[xi - 1][yi] != 9)
         {
             mov = 4;
-            move_left();
+            move_left(time);
             ret = 1;
         }
     return ret;
 
 }
 
-void Enemy::move(int p_xi, int p_yi)
+void Enemy::move(int p_xi, int p_yi, float time)
 {
-    if (mov > 10)
+    if (disgrace == 0)
     {
-        if (looking() == 1)
+        if (mov > 10)
         {
-            bool a228 = 0;
-            for (int i = 0; i < 4; i ++)
-                if (a228 == 0)
-                {
-                    int gw = good_way(p_xi, p_yi, i);
-                    a228 = move_simple(gw);
-                }
+            if (looking() == 1)
+            {
+                bool a228 = 0;
+                for (int i = 0; i < 4; i ++)
+                    if (a228 == 0)
+                    {
+                        int gw = good_way(p_xi, p_yi, i);
+                        a228 = move_simple(gw, time);
+                    }
+            }
+            else
+            {
+                mov -= 10;
+            }
         }
         else
         {
-            mov -= 10;
+            switch (mov) {
+                case 1:
+                    move_up(time);
+                    break;
+                case 2:
+                    move_down(time);
+                    break;
+                case 3:
+                    move_right(time);
+                    break;
+                case 4:
+                    move_left(time);
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
     else
     {
-        switch (mov) {
-            case 1:
-                move_up();
-                break;
-            case 2:
-                move_down();
-                break;
-            case 3:
-                move_right();
-                break;
-            case 4:
-                move_left();
-                break;
-
-            default:
-                break;
+        disgrace -= time;
+        if (mov > 10)
+        {
+            if (looking() == 1)
+            {
+                bool a228 = 0;
+                while (a228 == 0)
+                {
+                    a228 = move_simple(rand()%4 + 1,time);
+                }
+            }
+            else
+            {
+                mov -= 10;
+            }
         }
+        else
+        {
+            switch (mov) {
+                case 1:
+                    move_up(time);
+                    break;
+                case 2:
+                    move_down(time);
+                    break;
+                case 3:
+                    move_right(time);
+                    break;
+                case 4:
+                    move_left(time);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
     }
 }
 void Enemy::check_fail(double p_x, double p_y)
 {
     if(pow(p_x - x, 2) + pow(p_y - y, 2) < X*X/4)
     {
-        fail = 1;
+        if (disgrace == 0)
+        {
+            fail -= 1;
+            xi = 11;
+            yi = 11;
+        }
+        else
+            init((N - 1)/2, (N - 1)/2);
     }
 }
+
+#endif /* Enemy_h */
